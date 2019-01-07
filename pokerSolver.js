c09996aacccbdba3ec3997fec4475ee5e17428e7
waverly hinton <bobalink13@gmail.com>
2019-01-07 16:33:23 -0700

    Progress from interviewing Blake 1/07/19

--- pokerSolver.js
+++ pokerSolver.js
@@ -1,4 +1,4 @@
-var values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+var values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 var HandEnum = {
     ROYAL_FLUSH: 'Royal Flush',
     STRAIGHT_FLUSH: 'Straight flush',
@@ -36,6 +36,22 @@ class Card {
     }
 }
 
+function findDuplicates(valuesArray) {
+    let returnValues = {}
+
+    valuesArray.forEach(value => {
+        if (Object.keys(returnValues).indexOf(value) === -1) {
+            returnValues[value] = 1;
+        }
+        else {
+            returnValues[value] = returnValues[value] + 1
+        }
+    })
+
+    return returnValues;
+
+}
+
 
 /**
  * Build and return the value of the hand.
@@ -45,15 +61,81 @@ class Card {
 function solveHand(rawArray) {
     let handType = " ";
     let cardArray = [];
+    let suitArray = [];
+    let valueArray = [];
 
-    rawArray.forEach( (cardString)=> {
+    // defines arrays
+    rawArray.forEach( (cardString) => {
         const cardObj = new Card(cardString);
         cardArray.push(cardObj);
     });
+    suitArray = cardArray.map(card => card.suit)
+    valueArray = cardArray.map(card => card.value)
 
+    // find duplicates
+    let duplicateValueMap = findDuplicates(valueArray);
+    let duplicateSuitMap = findDuplicates(suitArray);
+    let pairExists = false;
+    let threeOfKindExists = false;
+    let isAStraight = false;
     cardArray = cardArray.sort(Card.sort);
 
-    handType = HandEnum.HIGH_CARD + ' ' +  cardArray[0];
+    for (const key in duplicateValueMap) {
+
+        switch (duplicateValueMap[key]) {
+            case 4:
+                handType = HandEnum.FOUR_OF_A_KIND
+                break;
+
+            case 3:
+                if (pairExists) {
+                    handType = HandEnum.FULL_HOUSE
+                }
+                else {
+                    handType = HandEnum.THREE_OF_A_KIND
+                }
+
+                threeOfKindExists = true;
+                break;
+
+            case 2:
+
+                if (pairExists) {
+                    handType = HandEnum.TWO_PAIR
+                }
+                else if (threeOfKindExists) {
+                    handType = HandEnum.FULL_HOUSE
+                }
+                else {
+                    handType = HandEnum.PAIR
+                }
+
+                pairExists = true
+                break;
+        }
+    }
+
+    isAStraight = cardArray.every((card, i, cardArray) => {
+        console.log(i)
+        if (i === 0) return true
+        else {
+            console.log(card.rank, cardArray[i - 1].rank)
+            if (card.rank === cardArray[i - 1].rank + 1) return true;
+            else return false;
+        }
+    } )
+
+    console.log('isAStraight', isAStraight)
+
+    if (Object.keys(duplicateSuitMap).length === 1) {
+        handType = HandEnum.FLUSH
+    }
+
+    if (handType === ' ') {
+        handType = HandEnum.HIGH_CARD + ' ' + cardArray[0];
+    }
+
+    console.log(handType)
 
     return handType
 }
